354a12858de6c0ed1720ca5ce25a88b32f497838
Rqq-al22 <rezkialya0909@gmail.com>
2025-12-13 20:01:58 +0800

    Create config.php

---
+++ backend/config.php
@@ -0,0 +1,4 @@
+$DB_HOST = getenv('LABIRIN_DB_HOST') ?: '127.0.0.1';
+$DB_NAME = getenv('LABIRIN_DB_NAME') ?: 'mpti_db';
+$DB_USER = getenv('LABIRIN_DB_USER') ?: 'root';
+$DB_PASS = getenv('LABIRIN_DB_PASS') ?: '';
